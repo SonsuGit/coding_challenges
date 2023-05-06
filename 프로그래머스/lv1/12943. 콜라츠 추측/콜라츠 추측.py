77b8ea52f0bdf3cc16e0c780b9d5621f088b857b
Suha Son <74539545+SonsuGit@users.noreply.github.com>
2023-05-06 12:31:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    cnt = 0
+    
+    if num == 1:
+        return 0
+    
+    while num != 1 and cnt < 500:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = 3 * num + 1
+        cnt += 1
+        
+    if cnt == 500:
+        return -1
+    
+    return cnt
