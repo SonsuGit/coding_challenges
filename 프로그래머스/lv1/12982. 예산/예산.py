771566622cd7be192a0fcf802487a0ba1571ce6d
Suha Son <74539545+SonsuGit@users.noreply.github.com>
2023-05-10 00:29:54 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -0,0 +1,11 @@
+def solution(d, budget):
+    d.sort()
+    result = 0
+    for i in d:
+        if budget - i >= 0:
+            budget -= i
+            result += 1
+        else:
+            break
+    
+    return result
