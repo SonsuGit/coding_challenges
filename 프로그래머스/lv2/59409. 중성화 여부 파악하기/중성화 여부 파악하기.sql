bf10370f13a936635ac55b6a23bf4195689cb20b
Suha Son <74539545+SonsuGit@users.noreply.github.com>
2023-06-30 16:39:21 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT 
+    animal_id,
+    name,
+    case 
+        when SEX_UPON_INTAKE like 'Neutered%' then 'O'
+        when SEX_UPON_INTAKE like 'spayed%' then 'O'
+        else 'X'
+        end as 중성화
+from animal_ins
